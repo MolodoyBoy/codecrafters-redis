0df7728c214d79b0b5d40ae6d2c57b0e335e38fb
o.chukhlian <oleg.chukhlian@pitchbook.com>
2025-12-09 16:05:31 +0200

    Handling Multiple Clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+
+    private static final String RESPONSE_MESSAGE = "+PONG\r\n";
 
     public static void main(String[] args) {
         System.out.println("Logs from your program will appear here!");
@@ -12,30 +15,39 @@ public class Main {
         int port = 6379;
 
         String pingCommand = "PING";
-        String responseMessage = "+PONG\r\n";
 
-        try (ServerSocket serverSocket = new ServerSocket()) {
+        try (ServerSocket serverSocket = new ServerSocket();
+             ExecutorService executorService = Executors.newFixedThreadPool(10)) {
+
             serverSocket.setReuseAddress(true);
             serverSocket.bind(new InetSocketAddress(port));
 
-            try (Socket clientSocket = serverSocket.accept();
-                 BufferedReader bufferedReader = getBufferedReader(clientSocket);
-                 BufferedWriter bufferedWriter = getBufferedWriter(clientSocket)) {
+            try (Socket clientSocket = serverSocket.accept()) {
+                executorService.submit(() -> handleClient(clientSocket));
+            }
 
-                while (true) {
-                    try {
-                        String readLine = bufferedReader.readLine();
-                        if (readLine.isBlank()) {
-                            continue;
-                        }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 
-                        if (readLine.equalsIgnoreCase("PING")) {
-                            bufferedWriter.write(responseMessage);
-                            bufferedWriter.flush();
-                        }
-                    } catch (IOException e) {
-                        System.out.println("IOException while handling client: " + e.getMessage());
+    private static void handleClient(Socket clientSocket) {
+        try (BufferedReader bufferedReader = getBufferedReader(clientSocket);
+             BufferedWriter bufferedWriter = getBufferedWriter(clientSocket)) {
+
+            while (true) {
+                try {
+                    String readLine = bufferedReader.readLine();
+                    if (readLine == null || readLine.isBlank()) {
+                        continue;
                     }
+
+                    if ("PING".equalsIgnoreCase(readLine)) {
+                        bufferedWriter.write(RESPONSE_MESSAGE);
+                        bufferedWriter.flush();
+                    }
+                } catch (IOException e) {
+                    System.out.println("IOException while handling client: " + e.getMessage());
                 }
             }
         } catch (IOException e) {
